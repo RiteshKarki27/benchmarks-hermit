0337e66f7d69d1bebeb3549c12c637c482c7ae05
Ritesh.K <riteshkarki6@gmail.com>
2026-06-16 23:29:44 +0200

    Implement fixed point arithmetic for load calculation
    Signed-off-by: Ritesh.K <riteshkarki6@gmail.com>

--- calculate_load/src/main.rs
+++ calculate_load/src/main.rs
@@ -1,10 +1,12 @@
-// use alloc::rc::Rc;
+#![allow(unused)]
 use nix::unistd::{sysconf, SysconfVar};
 // use procfs::process::Process;
 use procfs::{CurrentSI, KernelStats};
-use std::{collections::HashMap, sync::atomic::{AtomicU64, Ordering}, thread, time::{Duration, Instant}};
+use std::{collections::HashMap, ops::{Add, Mul, Sub}, sync::atomic::{AtomicU64, Ordering}, thread, time::{Duration, Instant}};
+use fixed::{FixedU32, traits::Fixed, types::{I9F23, extra::U23}};
 
 struct TaskId(i32);
+// Magic constants (to be later replaced by fixed point numbers)
 const EXP_1: f64 = 0.920044415;
 const EXP_5: f64 = 0.983471454;
 const EXP_15: f64 = 0.994459811;
@@ -30,56 +32,65 @@ const EXP_15: f64 = 0.994459811;
 // }
 #[derive(Default, Clone)]
 struct LoadAvg {
-    avg_1: f64,
-    avg_5: f64, 
-    avg_15: f64,
+    avg_1_fixed: FixedU32<U23>,
+    avg_5_fixed: FixedU32<U23>,
+    avg_15_fixed: FixedU32<U23>,
 }
 
 impl LoadAvg {
     fn new() -> Self {
-        Self { avg_1: 0.0, avg_5: 0.0, avg_15: 0.0}
+        Self { avg_1_fixed: FixedU32::ZERO, avg_5_fixed: FixedU32::ZERO, avg_15_fixed: FixedU32::ZERO}
     }
 
-    fn update_values(&mut self, active_tasks: f64) {
-        self.avg_1 = EXP_1 * self.avg_1 + (1.0 - EXP_1) * active_tasks;
-        self.avg_5 = EXP_5 * self.avg_5 + (1.0 - EXP_5) * active_tasks;
-        self.avg_15 = EXP_15 * self.avg_15 + (1.0 - EXP_15) * active_tasks;
+    fn update_values(&mut self, active_tasks: f64, exp_1_fixed: &FixedU32<U23>, exp_5_fixed: &FixedU32<U23>, exp_15_fixed: &FixedU32<U23>) {
+        // TODO: Write fixed point logic to update load values for every cycle. 
+        self.avg_1_fixed = FixedU32::mul(self.avg_1_fixed, exp_1_fixed)
+                                    .add((FixedU32::from_num(1.0).sub(exp_1_fixed))
+                                    .mul(FixedU32::from_num(active_tasks)));
+        self.avg_5_fixed = FixedU32::mul(self.avg_5_fixed, exp_5_fixed)
+                                    .add((FixedU32::from_num(1.0).sub(exp_5_fixed))
+                                    .mul(FixedU32::from_num(active_tasks)));
+        self.avg_15_fixed = FixedU32::mul(self.avg_15_fixed, exp_15_fixed)
+                                    .add((FixedU32::from_num(1.0).sub(exp_15_fixed))
+                                    .mul(FixedU32::from_num(active_tasks)));
     }
 }
 
-static count: AtomicU64 = AtomicU64::new(0);
+static COUNT: AtomicU64 = AtomicU64::new(0);
 
 fn increment() {
-    count.fetch_add(1, Ordering::Relaxed);
+    COUNT.fetch_add(1, Ordering::Relaxed);
 }
 
 fn get_count() -> u64 {
-    count.load(Ordering::Relaxed)
+    COUNT.load(Ordering::Relaxed)
 }
 
-fn returnNoCores() -> i64 {
+fn return_no_cores() -> i64 {
     sysconf(SysconfVar::_NPROCESSORS_ONLN).unwrap().unwrap()
 }
 
-fn listTasksWithCpu() -> procfs::ProcResult<()> {
-    for proc in procfs::process::all_processes()? {
-        let stat = proc.unwrap().stat()?;
-        if stat.state == 'R' || stat.state == 'D'{
-            println!("PID {} CPU {} CMD {}", stat.pid, stat.processor.unwrap_or(-1), stat.comm);
-            increment();
-        }
-    }
-    Ok(())
-}
+// fn list_tasks_with_cpu() -> procfs::ProcResult<()> {
+//     for proc in procfs::process::all_processes()? {
+//         let stat = proc.unwrap().stat()?;
+//         if stat.state == 'R' || stat.state == 'D'{
+//             println!("PID {} CPU {} CMD {}", stat.pid, stat.processor.unwrap_or(-1), stat.comm);
+//             increment();
+//         }
+//     }
+//     Ok(())
+// }
 
 fn active_tasks_per_cpu() -> procfs::ProcResult<HashMap<i32,u64>> {
-    let mut counts_per_cpu: HashMap<i32, u64> = HashMap::new();
+    // let mut counts_per_cpu: HashMap<i32, u64> = HashMap::new();
+    let mut counts_per_cpu: HashMap<i32, u64> = (0..return_no_cores() as i32).map(|c| (c, 0)).collect();
 
     for proc in procfs::process::all_processes()? {
-        let tasks = proc.unwrap().tasks()?;
+        let Ok(proc) = proc else {continue};
+        let tasks = proc.tasks()?;
         for task in tasks {
             let Ok(task) = task else {continue};
-            let task_stat = task.stat()?;
+            let Ok(task_stat) = task.stat() else { continue };
             if matches!(task_stat.state, 'R' | 'D') {
                 if let Some(cpu) = task_stat.processor {
                     *counts_per_cpu.entry(cpu).or_insert(0) += 1;
@@ -96,36 +107,34 @@ fn main() {
 
     let mut load_avg = LoadAvg::new();
 
-    while true {
+    let mut exp_1_fixed: FixedU32<U23> = FixedU32::from_num(EXP_1);
+    let mut exp_5_fixed: FixedU32<U23> = FixedU32::from_num(EXP_5);
+    let mut exp_15_fixed: FixedU32<U23> = FixedU32::from_num(EXP_15);
+    let mut per_cpu_load: Vec<LoadAvg> = vec![LoadAvg::default(); return_no_cores() as usize];
+
+    loop {
         // thread::sleep(Duration::from_secs(5));
         let no_cores = sysconf(SysconfVar::_NPROCESSORS_ONLN).unwrap().unwrap();
-        println!("No of cores {}", returnNoCores());
+        println!("No of cores {}", return_no_cores());
 
-        // let result = match listTasksWithCpu() {
-        //     Ok(()) => println!("Done."), 
-        //     Err(e) => eprintln!("Error with {e}"),
-        // };
-        // println!("Count: {}", get_count());
         let kstats  = procfs::KernelStats::current().unwrap();
         let active_procs = (kstats.procs_running.unwrap_or(0) + kstats.procs_blocked.unwrap_or(0)) as f64;
 
-        let mut per_cpu_load: Vec<LoadAvg> = vec![LoadAvg::default(); returnNoCores() as usize];
+        // let mut per_cpu_load: Vec<LoadAvg> = vec![LoadAvg::default(); return_no_cores() as usize];
         let mut next_tick = Instant::now();
 
-        load_avg.update_values(active_procs);
-        println!("load avg:  {:.2} {:.2} {:.2}", load_avg.avg_1, load_avg.avg_5, load_avg.avg_15);
-
         let count_tasks_per_cpu = active_tasks_per_cpu().unwrap();
-        
-        for cpu in 0..returnNoCores() {
+
+        for cpu in 0..return_no_cores() {
+            // println!("CPU: {}", cpu);
             let active_tasks = count_tasks_per_cpu.get(&(cpu as i32)).copied().unwrap_or(0) as f64;
-            per_cpu_load[cpu as usize].update_values(active_tasks);
+            per_cpu_load[cpu as usize].update_values(active_tasks, &exp_1_fixed, &exp_5_fixed, &exp_15_fixed);
+
         }
 
         for (cpu, load) in per_cpu_load.iter().enumerate() {
-            println!("load avg:  {:.2} {:.2} {:.2}", load.avg_1, load.avg_5, load.avg_15);
+            println!("load avg for {}: {:.2} {:.2} {:.2}", cpu, load.avg_1_fixed.to_num::<f32>(),load.avg_5_fixed.to_num::<f32>(),load.avg_15_fixed.to_num::<f32>());
         }
-
         next_tick += std::time::Duration::from_secs(5);
         let now = Instant::now();
         if next_tick > now {
